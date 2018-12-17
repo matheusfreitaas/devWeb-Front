86636a997ad56ced0ff8e70d2d0bb49a672d4e49
Matheus Freitas <matheusbfreitas@gmail.com>
2018-12-17 11:20:33 -0300

    Add Login

--- devweb/src/App.js
+++ devweb/src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import Evaluation from './Evaluation/Evaluation';
-import CardList from './Course/CardList'
+import Login from './Login';
 import Header from './Header'
+import Evaluation from './Evaluation/Evaluation'
+import CardList from './Course/CardList'
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header></Header>
-        <CardList></CardList>
+        <Router>
+          <div className="App">
+            <Header></Header>
+            <Route exact={true} path="/login" component={Login} />
+            <Route path="/courses" component={CardList}/>
+            <Route path="/evaluation" component={Evaluation}/>
+          </div>
+        </Router>
       </div>
     );
   }
