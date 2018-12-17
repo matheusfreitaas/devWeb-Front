4b65ddbb771701938b72ef133930633a8b72bf13
Matheus Freitas <matheusbfreitas@gmail.com>
2018-12-16 22:35:56 -0300

    add header

--- devweb/src/App.js
+++ devweb/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Evaluation from './Evaluation/Evaluation';
 import CardList from './Course/CardList'
+import Header from './Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <CardList></CardList>
+        <Header></Header>
+        <CardList></CardList>
       </div>
     );
   }
